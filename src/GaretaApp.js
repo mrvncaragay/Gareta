9dea458a8a492b233dac4474993612e81e50ab46
Marvin Caragay <mrvncaragay@gmail.com>
2019-09-02 00:47:49 -0700

    Init Gareta Project App

---
+++ src/GaretaApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+
+const GaretaApp = () => {
+  return <Typography variant='h1'>Hi There</Typography>;
+};
+
+export default GaretaApp;
---
+++ src/theme/overrides/MuiButton.js
@@ -0,0 +1,19 @@
+// Palette
+import palette from '../palette';
+
+export default {
+  outlined: {
+    backgroundColor: palette.common.white,
+    color: palette.common.muted
+  },
+  contained: {
+    backgroundColor: palette.common.white,
+    '&:hover': {
+      backgroundColor: palette.common.neutral
+    }
+  },
+  label: {
+    height: '14px',
+    fontSize: '10px'
+  }
+};
